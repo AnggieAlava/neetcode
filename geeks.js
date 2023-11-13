4f2255b38b00a3c3b30aea42718c306a65ebc1d6
Anggie Alava <anggie.alava.12@gmail.com>
2023-11-13 18:12:55 -0500

    foundOdds en geeks.js mofificado

--- geeks.js
+++ geeks.js
@@ -1,15 +1,27 @@
 function foundOdds(arr) {
-  let newArr = {};
-  for (let i of arr) {
-    if (!newArr.hasOwnProperty(i)) newArr[i] = 0;
-    newArr[i]++;
-  }
-  let result = [];
-  for (const [key, val] of Object.entries(newArr)) {
-    if (val % 2 != 0) result.push(key);
+  // Create a map to store the count of each number in the array
+  const countMap = new Map();
+
+  // Iterate over each number in the array
+  for (const num of arr) {
+    // Increment the count of the number in the map
+    countMap.set(num, (countMap.get(num) || 0) + 1);
   }
 
-  return result;
+  // Create an array to store the odd numbers
+  const oddNums = [];
+
+  // Iterate over each entry in the count map
+  for (const [key, val] of countMap.entries()) {
+    // Check if the count of the number is odd
+    if (val % 2 !== 0) {
+      // Add the number to the array of odd numbers
+      oddNums.push(key);
+    }
+  }
+
+  // Return the array of odd numbers
+  return oddNums;
 }
 let matriz = [7, 1, 1, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 5, 9];
 let output = foundOdds(matriz);
