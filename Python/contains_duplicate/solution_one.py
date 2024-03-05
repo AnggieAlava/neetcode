11fd75d4a4b79c401432f85374995fe7c804a89e
Anggie Alava <codewithangels@gmail.com>
2024-03-04 21:23:38 -0500

    Contains duplicate add second solution

---
+++ Python/contains_duplicate/solution_one.py
@@ -0,0 +1,10 @@
+def containsDuplicate(nums):
+    hashset = set()
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+
+
+print(containsDuplicate([1, 2, 3, 4, 5]))
