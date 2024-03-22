cea824adf03ed65dc802ceaad54c08de6384003b
Anggie Alava <codewithangels@gmail.com>
2024-03-22 18:46:10 -0500

    Agregad ejercicio concatenation of an array y valid anagram

---
+++ exercises/concatenation_of_an_array/solution.py
@@ -0,0 +1,5 @@
+def concatenation_of_an_array(nums):
+    return nums * 2
+
+
+print(concatenation_of_an_array([1, 2, 1]))
---
+++ exercises/valid_anagram/solution.py
@@ -0,0 +1,15 @@
+def valid_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    countS, countT = {}, {}
+
+    for i in range(len(s)):
+        countS[s[i]] = countS.get(s[i], 0) + 1
+        countT[t[i]] = countT.get(t[i], 0) + 1
+    for c in countS:
+        if countS[c] != countT.get(c, 0):
+            return False
+    return True
+
+
+print(valid_anagram("car", "rac"))
