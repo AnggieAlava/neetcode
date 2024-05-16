2b43df47e48b634dda8906bdd3b8ff3f1bf82fa4
Anggie Alava <codewithangels@gmail.com>
2024-05-15 22:59:08 -0500

    improved mi directory

--- exercises/contains_duplicate/other/other.js
+++
@@ -1,5 +0,0 @@
-function containsDuplicate(nums) {
-  return nums.length !== new Set(nums).size;
-}
-
-console.log(containsDuplicate([1, 2, 3, 4]));
--- exercises/contains_duplicate/other/other.py
+++
@@ -1,5 +0,0 @@
-def contains_duplicate(nums):
-    return len(nums) > len(set(nums))
-
-
-print(contains_duplicate([1, 2, 3, 4, 1]))
